576941993996ecfc28d6cfc1f9e34b6250a2f196
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-03-02 23:49:42 +0900

    for faster image loading
    'return' deleted
    image.src position moved to every first lines of funcitons

--- carousel.js
+++ carousel.js
@@ -1,17 +1,3 @@
-/*
-이미지 하단의 Dot를 누를 경우, 해당 순번의 이미지 보여주기
-
-dot = ul.li
-brainstorming for carousel
-
-1. dots -> ul> li, add or remove classlist 'active'
-2. arrows
-3. load first image -> ref. background.js // set as default, not form local storage.
-someImageElement.src = "/images/image-1.png";
-오른쪽 버튼 누르면 img number +1
-왼쪽 버튼 -1
-*/
-
 const container = document.querySelector(".container"),
     image = container.querySelector("img"),
     arrowLeft = container.querySelector(".arrow-left"),
@@ -50,38 +36,38 @@ function goRight(){
 function contronIndex(){
     const marker1 = document.getElementById("1");
     marker1.addEventListener("click", function(){
+        image.src = `images/image-1.jpg`;
         BUTTON.classList.remove(ACTIVE);
         BUTTON = document.getElementById("1");
         BUTTON.classList.add(ACTIVE);
-        return image.src = `images/image-1.jpg`;
     })
     const marker2 = document.getElementById("2");
     marker2.addEventListener("click", function(){
+        image.src = `images/image-2.jpg`;
         BUTTON.classList.remove(ACTIVE);
         BUTTON = document.getElementById("2");
         BUTTON.classList.add(ACTIVE);
-        return image.src = `images/image-2.jpg`;
     })
     const marker3 = document.getElementById("3");
     marker3.addEventListener("click", function(){
+        image.src = `images/image-3.jpg`;
         BUTTON.classList.remove(ACTIVE);
         BUTTON = document.getElementById("3");
         BUTTON.classList.add(ACTIVE);
-        return image.src = `images/image-3.jpg`;
     })
     const marker4 = document.getElementById("4");
     marker4.addEventListener("click", function(){
+        image.src = `images/image-4.jpg`
         BUTTON.classList.remove(ACTIVE);
         BUTTON = document.getElementById("4");
         BUTTON.classList.add(ACTIVE);
-        return image.src = `images/image-4.jpg`;
     })
     const marker5 = document.getElementById("5");
     marker5.addEventListener("click", function(){
+        image.src = `images/image-5.jpg`;
         BUTTON.classList.remove(ACTIVE);
         BUTTON = document.getElementById("5");
         BUTTON.classList.add(ACTIVE);
-        return image.src = `images/image-5.jpg`;
     })
 }
 
