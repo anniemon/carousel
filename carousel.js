1a106d7ae9bad0a1d658de2e5327699f08a09e73
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-03-03 12:31:24 +0900

    marker declaration position changed, painting image funciton splited

--- carousel.js
+++ carousel.js
@@ -1,8 +1,12 @@
 const container = document.querySelector(".container"),
     image = container.querySelector("img"),
     arrowLeft = container.querySelector(".arrow-left"),
-    arrowRight = container.querySelector(".arrow-right");
-
+    arrowRight = container.querySelector(".arrow-right"),
+    marker1 = document.getElementById("1"),
+    marker2 = document.getElementById("2"),
+    marker3 = document.getElementById("3"),
+    marker4 = document.getElementById("4"),
+    marker5 = document.getElementById("5");
 
 let Index = 1;
 const ACTIVE = "active";
@@ -32,47 +36,51 @@ function goRight(){
     BUTTON.classList.add(ACTIVE);
 }
 
-
-function contronIndex(){
-    const marker1 = document.getElementById("1");
-    marker1.addEventListener("click", function(){
-        image.src = `images/image-1.jpg`;
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("1");
-        BUTTON.classList.add(ACTIVE);
-    })
-    const marker2 = document.getElementById("2");
-    marker2.addEventListener("click", function(){
-        image.src = `images/image-2.jpg`;
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("2");
-        BUTTON.classList.add(ACTIVE);
-    })
-    const marker3 = document.getElementById("3");
-    marker3.addEventListener("click", function(){
-        image.src = `images/image-3.jpg`;
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("3");
-        BUTTON.classList.add(ACTIVE);
-    })
-    const marker4 = document.getElementById("4");
-    marker4.addEventListener("click", function(){
-        image.src = `images/image-4.jpg`
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("4");
-        BUTTON.classList.add(ACTIVE);
-    })
-    const marker5 = document.getElementById("5");
-    marker5.addEventListener("click", function(){
-        image.src = `images/image-5.jpg`;
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("5");
-        BUTTON.classList.add(ACTIVE);
-    })
+function paint1(){
+    image.src = `images/image-1.jpg`;
+    image.complete
+    BUTTON.classList.remove(ACTIVE);
+    BUTTON = document.getElementById("1");
+    BUTTON.classList.add(ACTIVE);
+}
+function paint2(){
+    image.src = `images/image-2.jpg`;
+    image.complete
+    BUTTON.classList.remove(ACTIVE);
+    BUTTON = document.getElementById("2");
+    BUTTON.classList.add(ACTIVE);
+}
+function paint3(){
+    image.src = `images/image-3.jpg`;
+    image.complete
+    BUTTON.classList.remove(ACTIVE);
+    BUTTON = document.getElementById("3");
+    BUTTON.classList.add(ACTIVE);
 }
 
+function paint4(){
+    image.src = `images/image-4.jpg`;
+    image.complete
+    BUTTON.classList.remove(ACTIVE);
+    BUTTON = document.getElementById("4");
+    BUTTON.classList.add(ACTIVE);
+}
+
+function paint5(){
+    image.src = `images/image-5.jpg`;
+    image.complete
+    BUTTON.classList.remove(ACTIVE);
+    BUTTON = document.getElementById("5");
+    BUTTON.classList.add(ACTIVE);
+}
+
+
 function init() {
-    contronIndex();
+    marker1.addEventListener("click", paint1);
+    marker2.addEventListener("click", paint2);
+    marker3.addEventListener("click", paint3);
+    marker4.addEventListener("click", paint4);
+    marker5.addEventListener("click", paint5);
     arrowLeft.addEventListener("click", goLeft);
     arrowRight.addEventListener("click", goRight);
 }
