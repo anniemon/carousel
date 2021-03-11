e6cad0ef021bed56e635e7da2cf9325c926915c0
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-03-11 23:20:37 +0900

    paint function refactoring
    function paint1~5 combined into function controlIndex...

--- carousel.js
+++ carousel.js
@@ -2,11 +2,7 @@ const container = document.querySelector(".container"),
     image = container.querySelector("img"),
     arrowLeft = container.querySelector(".arrow-left"),
     arrowRight = container.querySelector(".arrow-right"),
-    marker1 = document.getElementById("1"),
-    marker2 = document.getElementById("2"),
-    marker3 = document.getElementById("3"),
-    marker4 = document.getElementById("4"),
-    marker5 = document.getElementById("5");
+    marker = document.querySelectorAll('li');
 
 let INDEX = 1;
 const ACTIVE = "active";
@@ -42,61 +38,23 @@ function goRight(){
     })
 }
 
-function paint1(){
-    image.src = `images/image-1.jpg`;
-    image.decode()
-    .then(function(){
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("1");
-        BUTTON.classList.add(ACTIVE);
-        })
-}
-function paint2(){
-    image.src = `images/image-2.jpg`;
-    image.decode()
-    .then(function(){
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("2");
-        BUTTON.classList.add(ACTIVE);
-        })
-}
-function paint3(){
-    image.src = `images/image-3.jpg`;
-    image.decode()
-    .then(function(){
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("3");
-        BUTTON.classList.add(ACTIVE);
-        })
-}
-
-function paint4(){
-    image.src = `images/image-4.jpg`;
-    image.decode()
-    .then(function(){
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("4");
-        BUTTON.classList.add(ACTIVE);
-        })
-}
-
-function paint5(){
-    image.src = `images/image-5.jpg`;
-    image.decode()
-    .then(function(){
-        BUTTON.classList.remove(ACTIVE);
-        BUTTON = document.getElementById("5");
-        BUTTON.classList.add(ACTIVE);
-        })
-}
-
+function ControlIndex(){
+    marker.forEach(function(indicator, ind){
+        indicator.addEventListener('click', function(){
+            INDEX = ind+1;
+            image.src = `images/image-${INDEX}.jpg`;
+            image.decode()
+            .then(function(){
+                BUTTON.classList.remove(ACTIVE);
+                BUTTON = document.getElementById(INDEX);
+                BUTTON.classList.add(ACTIVE);
+            });
+        });
+    });
+};
 
 function init() {
-    marker1.addEventListener("click", paint1);
-    marker2.addEventListener("click", paint2);
-    marker3.addEventListener("click", paint3);
-    marker4.addEventListener("click", paint4);
-    marker5.addEventListener("click", paint5);
+    ControlIndex();
     arrowLeft.addEventListener("click", goLeft);
     arrowRight.addEventListener("click", goRight);
 }
